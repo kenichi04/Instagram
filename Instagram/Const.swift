84a2cd54b9c4948643cc9bc87263cf3290b64148
kenichi04 <slow_life_1407@yahoo.co.jp>
2021-01-10 17:47:58 +0900

    投稿機能の追加（FirebaseのFirestore, Storage内に保存）

---
+++ Instagram/Const.swift
@@ -0,0 +1,16 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by takashimakenichi on 2021/01/10.
+//  Copyright © 2021 takashimakenichi. All rights reserved.
+//
+
+import Foundation
+
+// Firebase関連の定数を管理する構造体
+struct Const {
+    static let ImagePath = "images"   // Strage内の画像ファイル保存場所
+    static let PostPath = "posts"     // Firebase内の投稿データ（投稿者、キャプション、投稿日時等）保存場所
+}
+
--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -26,20 +26,51 @@ class PostViewController: UIViewController {
     
     // 投稿ボタンタップ時
     @IBAction func handlePostButton(_ sender: Any) {
+        // 画像をjpeg形式に変換
+        let imageData = image.jpegData(compressionQuality: 0.75)
+        // 画像と投稿データの保存場所を定義
+        // Firestoreに保存する投稿データの保存場所
+        let postRef = Firestore.firestore().collection(Const.PostPath).document()
+        // Storageに保存する画像の保存場所
+        let imageRef = Storage.storage().reference().child(Const.ImagePath).child(postRef.documentID + ".jpg")
+        
+        // HUDで投稿処理中の表示を開始
+        SVProgressHUD.show()
+        // Storageに画像をアップロード
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        // putdataメソッドは、アップロード完了すると呼び出されるクロージャを最終引数に指定
+        imageRef.putData(imageData!, metadata: metadata) { (metadatam, error) in
+            if error != nil {
+                // 画像のアップロード失敗
+                print(error!)
+                SVProgressHUD.showError(withStatus: "画像のアップロードが失敗しました")
+                // 投稿をキャンセルし、先頭画面に戻る
+                UIApplication.shared.windows.first{ $0.isKeyWindow }?.rootViewController?.dismiss(animated: true, completion: nil)
+                return
+            }
+            // Firestoreに投稿データを保存
+            let name = Auth.auth().currentUser?.displayName
+            //
+            let postDic = [
+                "name": name!,
+                "caption": self.textField.text!,
+                "date": FieldValue.serverTimestamp(),  // Firestoreのサーバー上の時計を使用して日時を保存
+                ] as [String: Any]
+            // 保存するデータを辞書の形で引数に渡すことで、firestoreにデータを保存できる
+            postRef.setData(postDic)
+            
+            // HUDで投稿完了を表示
+            SVProgressHUD.showSuccess(withStatus: "投稿しました")
+            // 投稿が完了したので先頭画面に戻る
+            UIApplication.shared.windows.first{ $0.isKeyWindow }?.rootViewController?.dismiss(animated: true, completion: nil)
+        }
     }
     
     // キャンセルボタンタップ時
     @IBAction func handleCancelButton(_ sender: Any) {
+        // 加工画面に戻し、追加編集できるようにする
+        self.dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
